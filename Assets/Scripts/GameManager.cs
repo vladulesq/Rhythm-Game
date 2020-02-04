90f85271a70e6b69c67ebacd0fc135899710685a
Radulescu Vlad <vladulesq@gmail.com>
2020-02-04 20:02:09 +0200

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public BeatScroller theBS;
+    
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
+
+    public float totalNotes;
+    public float normalHits;
+    public float goodHits;
+    public float perfectHits;
+    public float missedHits;
+
+
+    public GameObject resultsScreen;
+
+    public Text percentHitText, normalsText, goodsText, perfectsText, missesText, rankText, finalScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        totalNotes = FindObjectsOfType<NoteObject>().Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+        else
+        {
+            if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            {
+                resultsScreen.SetActive(true);
+
+                normalsText.text = normalHits.ToString();
+                goodsText.text = goodHits.ToString();
+                perfectsText.text = perfectHits.ToString();
+                missesText.text = missedHits.ToString();
+
+                float totalHit = normalHits + goodHits + perfectHits;
+                float percentHit = (totalHit / totalNotes) * 100f;
+
+                percentHitText.text = percentHit.ToString("F1")+"%";
+
+                string rankValue = "F";
+                if (percentHit > 40)
+                {
+                    rankValue = "D";
+                    if (percentHit > 55)
+                    {
+                        rankValue = "C";
+                        if (percentHit > 70)
+                        {
+                            rankValue = "B";
+                            if (percentHit > 85)
+                            {
+                                rankValue = "A";
+                                if (percentHit > 95)
+                                {
+                                    rankValue = "S";
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+                rankText.text = rankValue;
+
+                finalScoreText.text = currentScore.ToString();
+                
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit on Time");
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+        //currentScore += scorePerNote *currentMultiplier;
+        scoreText.text = "Score: "+currentScore;
+    }
+
+    public void normalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+
+        normalHits++;
+    }
+
+    public void goodHit()
+    {
+        currentScore += scorePerGoodNote * multiplierTracker;
+        NoteHit();
+
+        goodHits++;
+    }
+
+    public void perfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
+        perfectHits++;
+    }
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        missedHits++;
+    }
+}
+
---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
+    public AudioSource soundEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed)
+            {
+                gameObject.SetActive(false);
+
+
+                if (Mathf.Abs(transform.position.y) > 0.25)
+                {
+                    Debug.Log("hit");
+                    GameManager.instance.normalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
+                else if(Mathf.Abs(transform.position.y)>0.05f)
+                {
+                    Debug.Log("good");
+                    GameManager.instance.goodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("perfect hit");
+                    GameManager.instance.perfectHit();
+                    soundEffect.Play();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                    
+                }
+                
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (gameObject.activeSelf == true)
+        {
+            if (other.tag == "Activator")
+            {
+                canBePressed = false;
+                GameManager.instance.NoteMissed();
+                Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+            }
+        }
+    }
+}
